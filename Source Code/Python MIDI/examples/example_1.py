931755e9848091bf36a7fd8f2a6a41e7139ad0af
ozkansafak <safakozkan77@gmail.com>
2016-06-19 21:38:30 -0700

    preprocessing.py created

--- Source Code/Python MIDI/examples/example_1.py
+++ Source Code/Python MIDI/examples/example_1.py
@@ -6,10 +6,10 @@ track = midi.Track()
 # Append the track to the pattern
 pattern.append(track)
 # Instantiate a MIDI note on event, append it to the track
-on = midi.NoteOnEvent(tick=0, velocity=20, pitch=midi.G_3)
+on = midi.NoteOnEvent(tick=0, velocity=100, pitch=midi.A_3)
 track.append(on)
 # Instantiate a MIDI note off event, append it to the track
-off = midi.NoteOffEvent(tick=100, pitch=midi.G_3)
+off = midi.NoteOffEvent(tick=200, velocity=50, pitch=midi.G_3)
 track.append(off)
 # Add the end of track event, append it to the track
 eot = midi.EndOfTrackEvent(tick=1)
@@ -18,3 +18,4 @@ track.append(eot)
 print pattern
 # Save the pattern to disk
 midi.write_midifile("example.mid", pattern)
+
